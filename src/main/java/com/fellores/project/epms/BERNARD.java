f876997395a580a2414ddc58b419be2dfb8e87bb
Jomar Fellores <Jomar Fellores@LAPTOP-2MRAT48F>
2024-07-19 23:50:01 +0800

    10 inch cock

--- src/main/java/com/fellores/project/epms/BERNARD.java
+++ src/main/java/com/fellores/project/epms/BERNARD.java
@@ -11,5 +11,6 @@ package com.fellores.project.epms;
 public class BERNARD {
     public static void main(String[] args) {
         System.out.println("wtf");
+        System.out.println("OMG");
     }
 }
